391f72a52612ae5f6e6c8f3cbdb4ae05a3cbffca
abdelnateron599-web <abdelnateron599@gmail.com>
2026-04-29 01:58:11 -0500

    Delete src directory

--- src/app/supabase.config.ts
+++
@@ -1,4 +0,0 @@
-export const supabaseConfig = {
-  url: 'https://xwbklksygssrvbyovjsv.supabase.co',
-  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh3Ymtsa3N5Z3NzcnZieW92anN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3ODAxMDcsImV4cCI6MjA5MjM1NjEwN30.2HaFP5_RbGB1kiidpYJ5F1yKyRGp8RqOY_tGq0QY4vc'
-};
